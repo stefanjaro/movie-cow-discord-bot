f048c8dcb563b30a937062d21632b49d6153f071
Stefan Jaro <stefanjaro@gmail.com>
2020-04-13 23:56:49 +0530

    bug fix handled long responses not printing

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import math
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
@@ -10,8 +11,40 @@ from omdb_movie_search import compile_movie_details_response_to_string, compile_
 # load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+CHARACTER_LIMIT = 2000
 
 bot = commands.Bot(command_prefix='moo')
+
+def split_long_response(response):
+    """
+    For messages longer than 2000 characters, split them by section first
+    and then by characters next if some of the sections are still too long
+    """
+    # split by section
+    response_split = response.split("\n\n")
+
+    # for storing the verified response sections
+    verified_response_split = []
+
+    for response_section in response_split:
+        # if each response section is less than the character limit, append to verified response split
+        if len(response_section) < CHARACTER_LIMIT:
+            verified_response_split.append(response_section)
+        
+        # if it's more, break the response sections that are longer than 2000 chars
+        else:
+            # figure out how many times we need to split it to get each part below 2000 and then split it by splicing
+            times_to_split = math.ceil(len(response_section) / CHARACTER_LIMIT)
+            split_response_section = [response_section[x * CHARACTER_LIMIT : (x+1) * CHARACTER_LIMIT].strip() for x in range(times_to_split)]
+
+            # the spoiler tags won't work when they're split so replace the double pipes and click to reveal in case they're in any of them
+            split_response_section = [x.replace("||", "").strip() for x in split_response_section]
+            split_response_section = [x.replace("(Click to Reveal)", "").strip() for x in split_response_section]
+
+            # store the entire split section
+            verified_response_split.extend(split_response_section)
+
+    return verified_response_split
 
 @bot.event
 async def on_ready():
@@ -39,8 +72,19 @@ async def movie_details_with_name(ctx, *args):
             response = compile_movie_details_response_to_string(api_response)
     else:
         response = "Contact the Developer. Tell him the API is having issues."
+  
+    # make sure the length of the response is less than discord's character limit
+    if len(response) < CHARACTER_LIMIT:
+        await ctx.send(response)
 
-    await ctx.send(response)
+    # if it's greater split the response and reply in chunks
+    else:
+        # split and verify lengths of sections
+        verified_response_split = split_long_response(response)
+
+        # reply in chunks
+        for verified_response_section in verified_response_split:
+            await ctx.send(verified_response_section)
 
 @bot.command(name="id", help='Returns movie information given an IMDb ID. Type mooname <IMDb ID>')
 async def movie_details_with_id(ctx, *args): 
@@ -60,7 +104,18 @@ async def movie_details_with_id(ctx, *args):
     else:
         response = "Contact the Developer. Tell him the API is having issues."
 
-    await ctx.send(response)
+    # make sure the length of the response is less than discord's character limit
+    if len(response) < CHARACTER_LIMIT:
+        await ctx.send(response)
+
+    # if it's greater split the response and reply in chunks
+    else:
+        # split and verify lengths of sections
+        verified_response_split = split_long_response(response)
+
+        # reply in chunks
+        for verified_response_section in verified_response_split:
+            await ctx.send(verified_response_section)
 
 @bot.command(name="search", help='Returns a list of movies given a movie name. Type moosearch <movie name>')
 async def movie_search(ctx, *args):
